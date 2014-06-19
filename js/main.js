0a1b07a2ca7c92067306e2ad39465659d34a1756
Simon Pratt <simon@pr4tt.com>
2014-06-18 21:07:54 -0400

    Style changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function() {
+	var github = github || {};
+
+	github.auth = function(id, secret) {
+		console.log(id, secret);
+	};
 	
+	$('#auth-button').click(function() {
+		var id = $('#auth-id').val();
+		var secret = $('#auth-secret').val();
+		github.auth(id, secret);
+	});
 });
